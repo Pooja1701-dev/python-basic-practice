98659bf7d699740a129bd37d39fb807abdec581c
Pooja1701-dev <nagapoojitha51@gmail.com>
2026-02-25 22:20:11 +0530

    factorial.py

---
+++ factorial.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number: "))
+fact = 1
+if num < 0:
+    print("Factorial not defined for negative numbers")
+else:
+    for i in range(1, num + 1):
+        fact = fact * i
+    print("Factorial is:", fact)
